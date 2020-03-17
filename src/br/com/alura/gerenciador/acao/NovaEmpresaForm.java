b01d639d1fe36484cd0950f2cd9208c95797fd99
Keilane Lais Borges Rizza Martins <kmartins@scopus.com.br>
2020-03-17 16:27:39 -0300

    formulario de login

--- src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
+++ src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
@@ -3,7 +3,7 @@ package br.com.alura.gerenciador.acao;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class NovaEmpresaForm {
+public class NovaEmpresaForm implements Acao {
 	
 	public String executa(HttpServletRequest request, HttpServletResponse respose) {
 		return "forward:formNovaEmpresa.jsp";
--- src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -9,12 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.alura.gerenciador.acao.AlteraEmpresa;
-import br.com.alura.gerenciador.acao.ListaEmpresas;
-import br.com.alura.gerenciador.acao.MostraEmpresa;
-import br.com.alura.gerenciador.acao.NovaEmpresa;
-import br.com.alura.gerenciador.acao.NovaEmpresaForm;
-import br.com.alura.gerenciador.acao.RemoveEmpresa;
+import br.com.alura.gerenciador.acao.Acao;
 
 @WebServlet("/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
@@ -23,32 +18,18 @@ public class UnicaEntradaServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String paramAcao = request.getParameter("acao");
-		String nome = null;
 		
-		if(paramAcao.equals("ListaEmpresas")) {
-			ListaEmpresas acao = new ListaEmpresas();
-			nome = acao.executa(request, response);
-			
-		} else if(paramAcao.equals("RemoveEmpresa")) {
-			RemoveEmpresa acao = new RemoveEmpresa();
-			nome = acao.executa(request, response);
-			
-		} else if(paramAcao.equals("MostraEmpresa")) {
-			MostraEmpresa acao = new MostraEmpresa();
-			nome = acao.executa(request, response);
-			
-		} else if(paramAcao.equals("AlteraEmpresa")) {
-			AlteraEmpresa acao = new AlteraEmpresa();
-			nome = acao.executa(request, response);
+		String className = "br.com.alura.gerenciador.acao." + paramAcao;
 		
-		} else if(paramAcao.equals("NovaEmpresa")) {
-			NovaEmpresa acao = new NovaEmpresa();
+		String nome;
+		try {
+			Class classe = Class.forName(className);
+			Acao acao = (Acao) classe.newInstance();
 			nome = acao.executa(request, response);
-		} else if(paramAcao.equals("NovaEmpresaForma")) {
-			NovaEmpresaForm acao = new NovaEmpresaForm();
-			nome = acao.executa(request, response);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new ServletException(e);
 		}
-		
+				
 		String[] a = nome.split(":");
 		
 		if(a[0].equals("forward")) {
